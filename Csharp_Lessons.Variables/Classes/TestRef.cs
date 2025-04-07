ab4c71bf065504cd96d870633dad1fe63490ab55
Alexandr Pekel <spekel2003@gmail.com>
2025-04-07 22:47:58 +0200

    Converted methods.

--- Csharp_Lessons.Variables/Classes/TestRef.cs
+++ Csharp_Lessons.Variables/Classes/TestRef.cs
@@ -5,8 +5,5 @@ public class TestRef
     public int X { get; set; }
     public int Y { get; set; }
 
-    public void Print()
-    {
-        Console.WriteLine(X + " " + Y);
-    }
+    public void Print() => Console.WriteLine(X + " " + Y);
 }
--- Csharp_Lessons.Variables/Classes/TestValue.cs
+++ Csharp_Lessons.Variables/Classes/TestValue.cs
@@ -4,9 +4,5 @@ public struct TestValue
 {
     public int X { get; set; }
     public int Y { get; set; }
-
-    public void Print()
-    {
-        Console.WriteLine(X + " " + Y);
-    }
+    public void Print() => Console.WriteLine(X + " " + Y);
 }
